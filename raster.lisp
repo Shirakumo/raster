a78798fce43ee502043a3eb2a5298b6e2bfd67d7
Shinmera <shinmera@tymoon.eu>
2024-07-13 15:26:50 +0200

    BLAAA

--- raster.lisp
+++ raster.lisp
@@ -1,25 +1,41 @@
 (in-package #:org.shirakumo.raster)
 
-(defun fill-rectangle (x y w h buffer bw bh &key fill)
-  (let ((color (ensure-fill fill)))
-    (declare (dynamic-extent color))
-    (composite-sdf color (constantly 0f0) w h buffer bw bh :tx x :ty y)))
+(defun draw-lines (points buffer bw bh &key sampler line-width line-style join-style cap-style feather-radius)
+  (let ((sampler (ensure-sampler sampler)))
+    (declare (dynamic-extent sampler))
+    ;; TODO: implement draw-lines
+    ))
 
-(defun fill-ellipse (x y w h buffer bw bh &key fill)
-  (let ((color (ensure-fill fill)))
-    (declare (dynamic-extent color))
-    (composite-sdf color (ellipse (/ w 2) (/ h 2) w h) w h buffer bw bh :tx x :ty y)))
+(defun draw-bezier (points buffer bw bh &key sampler line-width line-style join-style cap-style feather-radius)
+  (let ((sampler (ensure-sampler sampler)))
+    (declare (dynamic-extent sampler))
+    ;; TODO: implement draw-bezier
+    ))
 
-(defun outline-rectangle (x y w h s buffer bw bh &key fill)
-  (let ((color (ensure-fill fill)))
-    (declare (dynamic-extent color))
-    (composite-sdf color (subtract (rectangle (/ w 2) (/ h 2) w h)
-                                   (rectangle (/ w 2) (/ h 2) (- w s) (- h s)))
-                   w h buffer bw bh :tx x :ty y)))
+(defun draw-rectangle (x y w h buffer bw bh &key sampler line-width border-radius feather-radius)
+  (let ((sampler (ensure-sampler sampler)))
+    (declare (dynamic-extent sampler))
+    ;; TODO: implement draw-rectagle
+    ))
 
-(defun outline-ellipse (x y w h s buffer bw bh &key fill)
-  (let ((color (ensure-fill fill)))
-    (declare (dynamic-extent color))
-    (composite-sdf color (subtract (ellipse (/ w 2) (/ h 2) w h)
-                                   (ellipse (/ w 2) (/ h 2) (- w s) (- h s)))
-                   w h buffer bw bh :tx x :ty y)))
+(defun draw-ellipse (x y w h buffer bw bh &key sampler line-width feather-radius)
+  (let ((sampler (ensure-sampler sampler)))
+    (declare (dynamic-extent sampler))
+    ;; TODO: implement draw-ellipse
+    ))
+
+(defun draw-polygon (points buffer bw bh &key sampler)
+  (let ((sampler (ensure-sampler sampler)))
+    (declare (dynamic-extent sampler))
+    ;; TODO: implement draw-polygon
+    ))
+
+(defun draw-image (image x y buffer bw bh &key)
+  (composite (image-buffer image) (image-width image) (image-height image)
+             buffer bw bh :tx x :ty y))
+
+(defun draw-text (string font buffer &key size sampler valign halign direction wrap markup feather-radius)
+  (let ((sampler (ensure-sampler sampler)))
+    (declare (dynamic-extent sampler))
+    ;; TODO: implement draw-text
+    ))
