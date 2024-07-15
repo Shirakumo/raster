78c55c6528208a2754d1173cd66316c6e8886739
Shinmera <shinmera@tymoon.eu>
2024-07-15 22:48:44 +0200

    Some more docs

--- staple.ext.lisp
+++ staple.ext.lisp
@@ -1,1 +1,13 @@
 (defmethod staple:subsystems ((_ (eql (asdf:find-system "raster")))) ())
+
+(defpackage "raster-docs"
+  (:use #:cl)
+  (:local-nicknames
+   (#:raster #:org.shirakumo.raster)))
+
+(defclass page* (staple:simple-page)
+  ()
+  (:default-initargs :document-package (find-package "raster-docs")))
+
+(defmethod staple:page-type ((system (eql (asdf:find-system :raster))))
+  'page*)
